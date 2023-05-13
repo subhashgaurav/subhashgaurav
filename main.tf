72dfdf81d28b3877b687ec7fb75ee51285fa9711
Subhash Gaurav <gaurav.subhash9@gmail.com>
2023-05-13 14:48:54 +0530

    RG

--- main.tf
+++ main.tf
@@ -5,51 +5,10 @@ provider "azurerm"{
 }
 
 
-module "azure-nsg" {
-  source = "git::https://github.com/andrewCluey/terraform-azurerm-nsg.git"
+module "terraform-azurerm-rg" {
+  source = "git::https://github.com/subhashgaurav/terraform-azurerm-rg.git"
 
-  resource_group_name = "rg-temp"
+  rg_name = "rg-1"
   location = "West India"
-  nsg_name = "temp2"
-   security_rules      = [
-       {
-            name                       = "W32Time",
-            priority                   = "100"
-            direction                  = "Inbound"
-            access                     = "Allow"
-            protocol                   = "UDP"
-            source_port_range          = "*"
-            destination_port_range     = "123"
-            source_address_prefix      = "*"
-            destination_address_prefix = "*"
-        },
-        {
-            name                       = "TLS_IN",
-            priority                   = "110"
-            direction                  = "Inbound"
-            access                     = "Allow"
-            protocol                   = "TCP"
-            source_port_range          = "*"
-            destination_port_range     = "443"
-            source_address_prefix      = "*"
-            destination_address_prefix = "*"
-        },
-         {
-            name                       = "WEB_OUT",
-            priority                   = "120"
-            direction                  = "Outbound"
-            access                     = "Allow"
-            protocol                   = "TCP"
-            source_port_range          = "*"
-            destination_port_ranges    = ["80", "443"]
-            source_address_prefix      = "*"
-            destination_address_prefix = "*"
-        }
-    ]
-
-    tags = {"env" = "POC"}
-
-
-
-
+  
 }
