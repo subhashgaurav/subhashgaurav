f0a35a08d525da9775023445c75acbbef8a79721
Subhash Gaurav <gaurav.subhash9@gmail.com>
2023-05-13 15:04:19 +0530

    RG

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-provider "azurerm"{
-    features {}  
-    
-    
+provider "azurerm" {
+    features {
+      
+    }
+  
 }
 
-
 module "terraform-azurerm-rg" {
-  source = "git::https://github.com/subhashgaurav/terraform-azurerm-rg.git"
+    source = "git::https://github.com/subhashgaurav/terraform-azurerm-rg.git"
 
-  rg_name = "rg-1"
-  location = "West India"
+    rg_name = "rg-1"
+    location = "east us"
   
 }
