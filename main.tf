52883f28f72bcf01345e17caa43e920a2bc66516
Subhash Gaurav <gaurav.subhash9@gmail.com>
2023-05-15 10:02:11 +0530

    Subhash

--- main.tf
+++ main.tf
@@ -8,9 +8,10 @@ provider "azurerm" {
 module "terraform-azurerm-vNet" {
     source = "git::https://github.com/subhashgaurav/terraform-azurerm-vNet.git"
 
+
     rg_name = "rg-1"
     location = "east us"
     vNet_name = "vNet-1"
-    vNet_name = "subnet-1"
-  
+    subnet_name = "subnet-1"
+
 }
